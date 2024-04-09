7c4b7871ae230e74f6b76332d7ede070a23bb283
dozsaerik <dozsaerik@gmail.com>
2024-04-09 12:40:00 +0200

    entrypoint

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -6,12 +6,5 @@ composer install
 # Generate JWT key pair
 php bin/console lexik:jwt:generate-keypair
 
-php bin/console doctrine:database:drop --force -q
-
-php bin/console doctrine:database:create
-
-# Update database schema
-php bin/console doctrine:schema:update --force --complete
-
 # Start PHP-FPM
 php-fpm
